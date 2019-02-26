76b0d744b0e729eae3e3c10f8a5836355352bb17
natefrizz <33046318+natefrizz@users.noreply.github.com>
2019-02-26 00:56:58 -0500

    Update payments.js

--- public/javascripts/payments.js
+++ public/javascripts/payments.js
@@ -10,29 +10,6 @@
  * such as iDEAL, SOFORT, SEPA Direct Debit, and more.
  */
 
-// SIFT Implementation
-  var _user_id = 'nate_frisella'; // Set to the user's ID, username, or email address, or '' if not yet known.
-  var _session_id = 'unique_session_id'; // Set to a unique session ID for the visitor's current browsing session.
-
-  var _sift = window._sift = window._sift || [];
-  _sift.push(['_setAccount', '1245589ac9']);
-  _sift.push(['_setUserId', _user_id]);
-  _sift.push(['_setSessionId', _session_id]);
-  _sift.push(['_trackPageview']);
-
- (function() {
-   function ls() {
-     var e = document.createElement('script');
-     e.src = 'https://cdn.sift.com/s.js';
-     document.body.appendChild(e);
-   }
-   if (window.attachEvent) {
-     window.attachEvent('onload', ls);
-   } else {
-     window.addEventListener('load', ls, false);
-   }
- })();
-
 (async () => {
   'use strict';
 
